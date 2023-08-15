e2b56cef260c72f6b7075059881c9bbc50b94194
AcaciaLima <caciabad@gmail.com>
2023-08-15 15:43:38 -0300

    commit 01

---
+++ src/app/subtracao/subtracao.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubtracaoComponent } from './subtracao';
+
+
+
+@NgModule({
+  declarations: [
+    SubtracaoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SubtracaoModule { }
---
+++ src/app/subtracao/subtracao/subtracao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subtracao',
+  templateUrl: './subtracao.component.html',
+  styleUrls: ['./subtracao.component.css']
+})
+export class SubtracaoComponent {
+
+}
