260061f76112129a3bc19eb3135d2d1ef45a1224
AcaciaLima <caciabad@gmail.com>
2023-08-15 16:54:00 -0300

    pagina113

--- src/app/subtracao/subtracao/subtracao.component.ts
+++ src/app/subtracao/subtracao/subtracao.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-subtracao',
   templateUrl: './subtracao.component.html',
   styleUrls: ['./subtracao.component.css']
 })
-export class SubtracaoComponent {
+export class SubtracaoComponent implements OnInit {
 
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
